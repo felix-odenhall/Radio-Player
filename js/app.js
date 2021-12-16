1dd992dbf0be1e7a7f61881141081171b7ccf55f
Felix-Navidad <90831955+Felix-Navidad@users.noreply.github.com>
2021-12-16 19:57:57 +0100

    m

--- js/app.js
+++ js/app.js
@@ -12,37 +12,12 @@ async function createRadioLinks(){
                 createRadioLink.href = `./radioStation/?id=${channelsArray[i].id}`
                 createRadioLink.addEventListener('click', ()=>{
                 })
-                createRadioLink.innerHTML =`<img class="radio-logo-img" src="${channelsArray[i].image}" alt="Logo of the radio station ${channelsArray[i].name}" />`
+                createRadioLink.innerHTML =`<img class="radio-logo-img" src="${channelsArray[i].image}" alt="Logo of the radio station ${channelsArray[i].name}" /> </ br>
+                `
                 radioLinkContainer.appendChild(createRadioLink)
         }
     }
 }
-
-
-async function createLiveRadio(){
-    const id = JSON.parse(findQuery("id"))
-    const audioElement = document.querySelector(".liveAudio")
-    const channelImg = document.querySelector('.live-img')
-    const programs = await getApi()
-    
-    const nowPlaying = await nowPlayingApi(id)
-    const nowPlayingElement = document.querySelector(".playing-now")
-    programs.channels.filter((program) => {
-        if(program.id === id){
-            channelImg.src = program.image
-            audioElement.src = program.liveaudio.url
-            nowPlayingElement.innerHTML = nowPlaying.playlist.song 
-            ? nowPlaying.playlist.song.description: 
-            ""
-        }
-    })
-}
-
-function findQuery (param) {
-    const searchUrlParams = new URLSearchParams(window.location.search);
-    return searchUrlParams.get(param);
-};
-
 
 async function getApi() {
     let response = await fetch("https://api.sr.se/api/v2/channels?format=json&pagination=false");
--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,24 +1,88 @@
-document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
+// document.addEventListener("DOMContentLoaded", function() { startplayer(); }, false);
 
-var player;
-
-var player;
-
-function startplayer() 
-{
- player = document.getElementById('musicPlayer');
- player.controls = false;
+function playPause(){
+    const audio = document.querySelector("#musicPlayer")
+    const image = document.querySelector(".playPause-img")
+    if(audio.paused){
+        console.log("sound on")
+        audio.play()
+        image.src = "../assets/pause.png"
+    } else {
+        console.log("sound off")
+        audio.pause()
+        image.src = "../assets/play.png"
+    }
 }
 
-function playRadio() 
-{
- player.play();
-} 
-function pauseRadio() 
-{
- player.pause();
+function changeVolume(){
+    const audio = document.querySelector("#musicPlayer")
+    audio.volume = document.getElementById("change_vol").value;
 }
-function changeVolume()
-{
- player.volume=document.getElementById("change_vol").value;
+// let player;
+
+// function startplayer() {
+//     player = document.getElementById('musicPlayer');
+//     player.controls = false;
+// }
+
+// function playRadio() {
+//     if(player.isPlaying){
+//         player.play();
+//     } else {
+//         player.pause();
+//     }
+// } 
+// function pauseRadio() {
+//     player.pause();
+// }
+// function changeVolume(){
+//     player.volume = document.getElementById("change_vol").value;
+// }
+
+// function playPause() {
+//     let contolImg = document.querySelector(".multi-btn")
+//     if (audio.isPlaying) {
+//         audio.pause()
+//         contolImg.src = "../assets/pause.png"
+//     } else {
+//         audio.play()
+//         contolImg.src = "../assets/play.png"
+//     }
+// }
+
+// function playPause(){
+
+//     if (player.isPlaying) {
+//         player.pause()
+//     controlImg.img.src = "../image/pause.png"
+//     } else {
+//         player.play()
+//     controlImg.img.src = "../image/play.png"
+//     }
+//     }
+
+
+
+async function createLiveRadio(){
+    const id = JSON.parse(findQuery("id"))
+    const audioElement = document.querySelector(".liveAudio")
+    const channelImg = document.querySelector('.live-img')
+    const programs = await getApi()
+    
+    const nowPlaying = await nowPlayingApi(id)
+    const nowPlayingElement = document.querySelector(".playing-now")
+    programs.channels.filter((program) => {
+        if(program.id === id){
+            channelImg.src = program.image
+            audioElement.src = program.liveaudio.url
+            nowPlayingElement.innerHTML = nowPlaying.playlist.song 
+            ? nowPlaying.playlist.song.description: 
+            ""
+        }
+    })
 }
+
+function findQuery (param) {
+    const searchUrlParams = new URLSearchParams(window.location.search);
+    return searchUrlParams.get(param);
+};
