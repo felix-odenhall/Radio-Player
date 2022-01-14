ddb8fd221c0456d58b28f99beacbd15847e4341c
Felix-Navidad <90831955+Felix-Navidad@users.noreply.github.com>
2022-01-14 15:41:55 +0100

    added the code from radioplayer.js to app.js

--- js/app.js
+++ js/app.js
@@ -46,6 +46,53 @@ function createRadioLinkImg(i, radioLinkContainer, channelsArray) {
   radioLinkContainer.appendChild(createRadioLink);
 }
 
+const audio = document.querySelector("#music-player");
+
+function playPause() {
+  const playPauseImg = document.querySelector(".playPause-img");
+  if (audio.paused) {
+    audio.play();
+    playPauseImg.src = "../assets/pause.png";
+  } else {
+    audio.pause();
+    playPauseImg.src = "../assets/play.png";
+  }
+}
+
+function changeVolume() {
+  audio.volume = document.getElementById("control-volume").value;
+}
+
+async function populateRadioPlayer() {
+  const id = JSON.parse(retriveUrl("id"));
+  const audioElement = document.querySelector(".live-audio");
+  const channelImg = document.querySelector(".live-img");
+  const programs = await getApi();
+
+  const nowPlaying = await nowPlayingApi(id);
+  const nowPlayingElement = document.querySelector(".playing-now");
+  programs.channels.filter((program) => {
+    if (program.id === id) {
+      channelImg.src = program.image;
+      channelImg.alt = `Logo of the radio station ${program.name}`;
+      audioElement.src = program.liveaudio.url;
+      nowPlayingElement.innerHTML = nowPlaying.playlist.song
+        ? nowPlaying.playlist.song.description
+        : "";
+    }
+  });
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+//PARAM IS A BAD VARIABLE NAME // Sorted - changed param to id
+function retriveUrl(id) {
+  const searchUrlParams = new URLSearchParams(window.location.search);
+  return searchUrlParams.get(id);
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+
 // Feedback (just for JavaScript):
 
 // ----- Sorted
--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,46 +1,46 @@
-const audio = document.querySelector("#music-player");
+// const audio = document.querySelector("#music-player");
 
-function playPause() {
-  const playPauseImg = document.querySelector(".playPause-img");
-  if (audio.paused) {
-    audio.play();
-    playPauseImg.src = "../assets/pause.png";
-  } else {
-    audio.pause();
-    playPauseImg.src = "../assets/play.png";
-  }
-}
+// function playPause() {
+//   const playPauseImg = document.querySelector(".playPause-img");
+//   if (audio.paused) {
+//     audio.play();
+//     playPauseImg.src = "../assets/pause.png";
+//   } else {
+//     audio.pause();
+//     playPauseImg.src = "../assets/play.png";
+//   }
+// }
 
-function changeVolume() {
-  audio.volume = document.getElementById("control-volume").value;
-}
+// function changeVolume() {
+//   audio.volume = document.getElementById("control-volume").value;
+// }
 
-async function populateRadioPlayer() {
-  const id = JSON.parse(retriveUrl("id"));
-  const audioElement = document.querySelector(".live-audio");
-  const channelImg = document.querySelector(".live-img");
-  const programs = await getApi();
+// async function populateRadioPlayer() {
+//   const id = JSON.parse(retriveUrl("id"));
+//   const audioElement = document.querySelector(".live-audio");
+//   const channelImg = document.querySelector(".live-img");
+//   const programs = await getApi();
 
-  const nowPlaying = await nowPlayingApi(id);
-  const nowPlayingElement = document.querySelector(".playing-now");
-  programs.channels.filter((program) => {
-    if (program.id === id) {
-      channelImg.src = program.image;
-      channelImg.alt = `Logo of the radio station ${program.name}`;
-      audioElement.src = program.liveaudio.url;
-      nowPlayingElement.innerHTML = nowPlaying.playlist.song
-        ? nowPlaying.playlist.song.description
-        : "";
-    }
-  });
-}
+//   const nowPlaying = await nowPlayingApi(id);
+//   const nowPlayingElement = document.querySelector(".playing-now");
+//   programs.channels.filter((program) => {
+//     if (program.id === id) {
+//       channelImg.src = program.image;
+//       channelImg.alt = `Logo of the radio station ${program.name}`;
+//       audioElement.src = program.liveaudio.url;
+//       nowPlayingElement.innerHTML = nowPlaying.playlist.song
+//         ? nowPlaying.playlist.song.description
+//         : "";
+//     }
+//   });
+// }
 
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+// // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
-//PARAM IS A BAD VARIABLE NAME // Sorted - changed param to id
-function retriveUrl(id) {
-  const searchUrlParams = new URLSearchParams(window.location.search);
-  return searchUrlParams.get(id);
-}
+// //PARAM IS A BAD VARIABLE NAME // Sorted - changed param to id
+// function retriveUrl(id) {
+//   const searchUrlParams = new URLSearchParams(window.location.search);
+//   return searchUrlParams.get(id);
+// }
 
-//https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+// //https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
